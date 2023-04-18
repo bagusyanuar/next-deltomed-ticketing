f9b0f014e98bb77b4d2b63844e7bb893252f6bb0
bagusyanuar <50134851+bagusyanuar@users.noreply.github.com>
2023-04-18 14:45:58 +0700

    abc

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,31 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { withIronSessionSsr } from 'iron-session/next'
-import { ironSessionOptions } from '../lib/session'
+import {
+  withIronSessionSsr
+} from 'iron-session/next'
+import {
+  ironSessionOptions
+} from '../lib/session'
 
 import Login from '../components/layouts/login'
 
 export default function Home() {
-  return (<Login />)
+  return ( < Login / > )
 }
 
-export const getServerSideProps = withIronSessionSsr(async function getServerSideProps({ req }) {
+export const getServerSideProps = withIronSessionSsr(async function getServerSideProps({
+  req
+}) {
   const token = req.session.token;
   if (token) {
-      return {
-          redirect: {
-              destination: '/dashboard',
-              permanent: true
-          }
+    return {
+      redirect: {
+        destination: '/dashboard',
+        permanent: true
       }
+    }
   }
-  return
+  return {
+    props: {},
+  }
 }, ironSessionOptions);
