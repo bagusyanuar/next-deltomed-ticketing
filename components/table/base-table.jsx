c3b5c6a82e27feba0a485afa4aa65a35facd028d
bagusyanuar <bagus.yanuar613@gmail.com>
2023-04-18 13:39:34 +0700

    pagination base table

--- components/table/base-table.jsx
+++ components/table/base-table.jsx
@@ -1,16 +1,48 @@
 
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 function BaseTable({ data }) {
 
-    const [perPage, setPerPage] = useState(5)
+    const [perPage, setPerPage] = useState(1)
     const [page, setPage] = useState(1)
+    const [countPage, setCountPage] = useState(0)
 
     const handleChangePerpage = (e) => {
         let perPage = parseInt(e.target.value);
         setPerPage(perPage)
-        console.log(e.target.value);
     }
+
+    const handleChangePage = (e) => {
+        e.preventDefault();
+        let targetPage = parseInt(e.target.dataset.page);
+        setPage(targetPage)
+        console.log(e.target.dataset.page);
+    }
+
+    const handleNextPage = (e) => {
+        e.preventDefault();
+        if (page < countPage) {
+            setPage(page + 1);
+        }
+    }
+
+    const handlePreviousPage = (e) => {
+        e.preventDefault();
+        if (page > 1) {
+            setPage(page - 1);
+        }
+    }
+    useEffect(() => {
+        console.log('affected');
+        let dataLength = data.length;
+        let totalPage = Math.ceil(dataLength / perPage);
+        setCountPage(totalPage)
+        return () => {
+            setCountPage(0)
+        }
+    }, [perPage, data.length])
+
+
     return (
         <div>
 
@@ -18,6 +50,8 @@ function BaseTable({ data }) {
             <div className='flex items-center mb-3'>
                 <span className='text-sm text-slate-600 me-2'>Show :</span>
                 <select onChange={handleChangePerpage} id='page_length' className='px-3 py-1 rounded-md border bg-inherit text-sm text-slate-600'>
+                    <option className='bg-white text-slate-600 hover:bg-slate-200' value={1}>1</option>
+                    <option className='bg-white text-slate-600 hover:bg-slate-200' value={2}>2</option>
                     <option className='bg-white text-slate-600 hover:bg-slate-200' value={5}>5</option>
                     <option className='bg-white text-slate-600 hover:bg-slate-200' value={10}>10</option>
                     <option className='bg-white text-slate-600 hover:bg-slate-200' value={25}>25</option>
@@ -43,10 +77,10 @@ function BaseTable({ data }) {
                     </thead>
                     <tbody>
                         {
-                            data.slice((page - 1), ((page - 1) + perPage)).map((value, index) => {
+                            data.slice(((page - 1) * perPage), (page * perPage)).map((value, index) => {
                                 return (
                                     <tr key={index} className='bg-white border-b'>
-                                        <td className={`px-6 py-3 text-gray-500 whitespace-nowrap w-1`}>{index + 1}</td>
+                                        <td className={`px-6 py-3 text-gray-500 whitespace-nowrap w-1`}>{((index + 1) + ((page - 1) * perPage))}</td>
                                         <td className={`px-6 py-3 text-gray-500 whitespace-nowrap`}>{value['name']}</td>
                                         <td className={`px-6 py-3 text-gray-500 whitespace-nowrap w-3`}>action</td>
                                     </tr>
@@ -83,33 +117,29 @@ function BaseTable({ data }) {
 
             {/* pagination section */}
             <nav className='flex items-center justify-between pt-4'>
-                <span className='text-sm text-slate-600 '>Showing <span className='font-semibold'>1-2</span> of <span className='font-semibold'>2</span>
+                <span className='text-sm text-slate-600 '>Showing <span className='font-semibold'>{((page - 1) * perPage) + 1}-{(((page - 1) * perPage) + perPage) > data.length ? data.length : (((page - 1) * perPage) + perPage)}</span> of <span className='font-semibold'>{data.length}</span>
                 </span>
                 <ul className='inline-flex items-center -space-x-px'>
                     <li>
-                        <a href='#' className='px-3 py-2 ml-0 leading-tight text-sm text-slate-600 border border-slate-300 rounded-l-md'>
+                        <a href='#' onClick={handlePreviousPage} className='px-3 py-2 ml-0 leading-tight text-sm text-slate-600 border border-slate-300 rounded-l-md'>
                             <span className="material-symbols-outlined text-sm">
                                 chevron_left
                             </span>
                         </a>
                     </li>
+                    {
+                        Array.from(Array(countPage)).map((value, index) => {
+                            return (
+                                <li key={index} className=''>
+                                    <a href='#' onClick={handleChangePage} data-page={index + 1} className={`${page === (index + 1) ? 'bg-green-500 text-white border-green-500' : 'text-slate-600 border-slate-300'} text-sm  px-3 py-2 leading-tight border`}>
+                                        {index + 1}
+                                    </a>
+                                </li>
+                            );
+                        })
+                    }
                     <li>
-                        <a href='#' className='text-sm text-slate-600 px-3 py-2 leading-tight border border-slate-300'>
-                            1
-                        </a>
-                    </li>
-                    <li>
-                        <a href='#' className='text-sm text-slate-600 px-3 py-2 leading-tight border border-slate-300'>
-                            2
-                        </a>
-                    </li>
-                    <li>
-                        <a href='#' className='text-sm text-slate-600 px-3 py-2 leading-tight border border-slate-300'>
-                            3
-                        </a>
-                    </li>
-                    <li>
-                        <a href='#' className='px-3 py-2 ml-0 leading-tight text-sm text-slate-600 border border-slate-300 rounded-r-md'>
+                        <a href='#' onClick={handleNextPage} className='px-3 py-2 ml-0 leading-tight text-sm text-slate-600 border border-slate-300 rounded-r-md'>
                             <span className="material-symbols-outlined text-sm">
                                 chevron_right
                             </span>
